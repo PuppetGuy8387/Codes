bf426c2ab9765e0becdc41befe87a406126a6a96
Dylan Anderson <10975309@uvu.edu>
2024-03-21 15:34:46 -0600

    Practice

---
+++ Lab1Practice.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Dylan";
+      Console.WriteLine(name);
+       int myNum = 15;
+      Console.WriteLine(myNum);
+     }
+  }
+}
